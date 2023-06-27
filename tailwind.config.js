284fa2f1886a5b0717e9b39ea0ff4083c0f5837b
smarthug <kirklayer@gmail.com>
2023-06-27 09:51:09 +0900

    tailwind sideNav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+  ],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
